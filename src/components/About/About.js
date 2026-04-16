85f13ceb4ce96e0ed3a3a6725baeb3f3b739f4f4
todanh2706 <thdanh23@clc.fitus.edu.vn>
2026-04-16 23:14:15 +0700

    Remove about image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,6 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
 
 function About() {
@@ -33,7 +32,6 @@ function About() {
               style={{ paddingTop: "120px", paddingBottom: "50px" }}
               className="about-img"
             >
-              <img src={laptopImg} alt="about" className="img-fluid" />
             </Col>
           </Row>
           <h1 className="project-heading">
